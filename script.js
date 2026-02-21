b888992c33073663f7ad07b3793856a53caccb12
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-02-21 22:51:36 +0600

    Implement Count

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const rejectedBtn = document.getElementById('rejectedBtn')
 const interviewBtn = document.getElementById('interviewBtn')
 const allJobs = document.getElementById('allJobBtn')
 
+
 function changeActiveColor(id) {
     rejectedBtn.classList.remove('bg-blue-600', 'text-white')
     interviewBtn.classList.remove('bg-blue-600', 'text-white')
@@ -11,7 +12,21 @@ function changeActiveColor(id) {
     interviewBtn.classList.add('bg-white', 'text-black')
     allJobs.classList.add('bg-white', 'text-black')
 
-   const selected = document.getElementById(id)
-   selected.classList.remove('bg-white', 'text-black')
-   selected.classList.add('bg-blue-600', 'text-white')
+    const selected = document.getElementById(id)
+    selected.classList.remove('bg-white', 'text-black')
+    selected.classList.add('bg-blue-600', 'text-white')
 }
+
+let jobsCard = document.getElementById('jobsCard')
+let totalCount = document.getElementById('totalCount')
+let interviewCount = document.getElementById('interviewCount')
+let rejectedCount = document.getElementById('rejectedCount')
+let interview = []
+let rejected = []
+
+function counter() {
+    totalCount.innerText = jobsCard.children.length
+    interviewCount.innerText = interview.length
+    rejectedCount.innerText = rejected.length
+}
+counter()
